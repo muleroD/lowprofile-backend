1950eab007d6237cb3827840f17e415b469348b0
Richard Mulero <mulerolp@gmail.com>
2022-01-29 17:04:38 -0300

    init: quarkus initializer

---
+++ src/main/java/br/coom/muler/GreetingConfig.java
@@ -0,0 +1,12 @@
+package br.coom.muler;
+
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithName;
+
+@ConfigMapping(prefix = "greeting")
+public interface GreetingConfig {
+
+    @WithName("message")
+    String message();
+
+}
---
+++ src/test/java/br/coom/muler/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package br.coom.muler;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
